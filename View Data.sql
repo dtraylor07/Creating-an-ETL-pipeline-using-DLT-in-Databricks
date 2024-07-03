92e14f841f7a0aca7f295afd0670789aba753722
dtraylor07 <98284895+dtraylor07@users.noreply.github.com>
2024-07-03 13:17:14 -0500

    Update View Data.sql

--- View Data.sql
+++ View Data.sql
@@ -1,18 +1,18 @@
 -- Use target schema -- 
 USE hive_metastore.dlt_pipeline_guide_target
 
--- View bronze layer -- 
+-- View bronze layer table  -- 
 SELECT * FROM delta_guide_bronze
 
--- See data attributes of bronze layer -- 
+-- See data attributes of bronze layer table -- 
 DESCRIBE delta_guide_bronze
 
--- View silver layer --
+-- View silver layer data table --
 SELECT * FROM delta_guide_silver
 
--- Change ordering of grades in silver layer -- 
+-- Change ordering of grades in silver layer table -- 
 SELECT * FROM delta_guide_silver
 ORDER BY student_grade
 
--- View gold layer -- 
+-- View gold layer table -- 
 SELECT * FROM delta_guide_gold
